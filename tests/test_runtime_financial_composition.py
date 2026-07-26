6ea5e60a64fab9397176c3e1268247b38579863d
NIMHunt <300551717+NIMHunt@users.noreply.github.com>
2026-07-26 15:55:19 +0100

    Update financial runtime composition coverage

--- tests/test_runtime_financial_composition.py
+++ tests/test_runtime_financial_composition.py
@@ -6,7 +6,7 @@ import sys
 import tempfile
 
 
-def test_production_runtime_installs_financial_guards_but_keeps_core_deposit_recorder():
+def test_production_runtime_installs_financial_guards_and_refund_address_safety():
     with tempfile.NamedTemporaryFile(suffix=".db") as db_file:
         env = {
             **os.environ,
@@ -19,7 +19,8 @@ def test_production_runtime_installs_financial_guards_but_keeps_core_deposit_rec
 import main
 import public_html
 import trans_updater
-assert trans_updater.record_spot_deposit_transaction.__module__ == 'trans_updater'
+assert trans_updater.record_spot_deposit_transaction.__module__ == 'refund_address_safety'
+assert trans_updater.resolve_nimiq_pay_payout_address.__module__ == 'refund_address_safety'
 assert trans_updater.submit_spot_cancellation_transactions.__module__ == 'cancellation_safety'
 assert trans_updater.submit_spot_creation_fee_transaction.__module__ == 'funding_fee_worker'
 assert public_html._deposit_summary.__module__ == 'funding_status'

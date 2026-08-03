cdc999aac000445eaf359f6fe505da1e6338343f
NIMHunt <300551717+NIMHunt@users.noreply.github.com>
2026-08-03 17:18:52 +0100

    Load Find Spots initial view correction

--- static/find_spots_bootstrap.js
+++ static/find_spots_bootstrap.js
@@ -1,2 +1,3 @@
 import './find_spots_search_transport_install.js?v=wrapped-search-v1-20260803';
-import './find_spots.js?v=wrapped-search-v1-20260803';
+import './find_spots_initial_view_install.js?v=initial-view-v1-20260803';
+import './find_spots.js?v=wrapped-search-v2-20260803';

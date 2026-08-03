739de8e5f19c89eab35b64b7d5d514d13228a6d3
NIMHunt <300551717+NIMHunt@users.noreply.github.com>
2026-08-03 15:58:13 +0100

    Preserve Find Spots module order

--- static/find_spots_bootstrap.js
+++ static/find_spots_bootstrap.js
@@ -1,7 +1,2 @@
-import { installFindSpotsSearchTransport } from './find_spots_search_transport.js?v=wrapped-search-v1-20260803';
-
-installFindSpotsSearchTransport();
-
-void import('./find_spots.js?v=wrapped-search-v1-20260803').catch((error) => {
-    console.error('NimHunt could not start the Find Spots page.', error);
-});
+import './find_spots_search_transport_install.js?v=wrapped-search-v1-20260803';
+import './find_spots.js?v=wrapped-search-v1-20260803';

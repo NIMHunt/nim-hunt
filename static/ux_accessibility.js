412967ca94cf7aeb0d4f1bd7383383c1a1724caf
NIMHunt <300551717+NIMHunt@users.noreply.github.com>
2026-08-24 17:02:50 +0100

    Refresh Demo Spot button module cache

--- static/ux_accessibility.js
+++ static/ux_accessibility.js
@@ -1,6 +1,6 @@
 import { installCreateSpotProgressiveSettings } from './create_spot_progressive.js?v=create-form-layout-v2-20260818';
 import { installFindSpotsDemo } from './find_spots_demo.js?v=demo-distance-250m-v1-20260818';
-import { installFindSpotsCreateCtaGuard } from './find_spots_user_cta.js?v=demo-inline-button-v1-20260823';
+import { installFindSpotsCreateCtaGuard } from './find_spots_user_cta.js?v=demo-button-polish-v2-20260824';
 import { installFindSpotsCreateModal } from './find_spots_create.js?v=demo-onboarding-v1-20260818';
 
 installCreateSpotProgressiveSettings();
